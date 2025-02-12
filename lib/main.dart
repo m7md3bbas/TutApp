ccc40c7d076153c6ae1e77b877924db70cf8a049
m7md3bbas <mhmdabass330@gmail.com>
2025-02-12 20:00:04 +0200

    adding color manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tutapp/app/my_app.dart';
 
 void main() {
-  Test1().updateCounter();
-  Test2().printCounter();
-}
-
-class MyApp extends StatefulWidget {
-  int counter = 0;
-
-  MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
-
-class Test1 {
-  void updateCounter() {
-    MyApp().counter = 13;
-  }
-}
-
-class Test2 {
-  void printCounter() {
-    print(MyApp().counter);
-  }
+  runApp(MyApp());
 }
---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primaryColor = Color(0xFFED9728);
+  static Color darkGreyColor = Color(0xFF525252);
+  static Color greyColor = Color(0xFF737477);
+  static Color lightGreyColor = Color(0xFF9E9E9E);
+}
