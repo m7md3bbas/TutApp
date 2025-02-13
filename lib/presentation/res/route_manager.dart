e65d64142e38331bd443111f528263197cf9bd52
m7md3bbas <mhmdabass330@gmail.com>
2025-02-13 03:51:53 +0200

    route generetor done

--- lib/presentation/res/route_manager.dart
+++ lib/presentation/res/route_manager.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tutapp/presentation/forgetpassword/forgetpassword_view.dart';
+import 'package:tutapp/presentation/login/login_view.dart';
+import 'package:tutapp/presentation/main/main_view.dart';
+import 'package:tutapp/presentation/register/register_view.dart';
+import 'package:tutapp/presentation/splash/splash_view.dart';
+import 'package:tutapp/presentation/store/store_view.dart';
+
 class RouteManager {
   static const String splashRoute = '/';
   static const String loginRoute = '/login';
@@ -5,4 +13,36 @@ class RouteManager {
   static const String forgetPasswordRoute = '/forgetpassword';
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storedetails';
+}
+
+class RouteGen {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteManager.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case RouteManager.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case RouteManager.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case RouteManager.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetpasswordView());
+      case RouteManager.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case RouteManager.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text("No Route Found"), // todo no hard code
+              ),
+              body: const Center(
+                  child: Text("No Route Found")), // todo no hard code
+            ));
+  }
 }
